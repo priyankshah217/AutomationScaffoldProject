eb560bb6888cc08ab44d390a28a6b2082f3949ac
Priyank Shah <priyank.shah@coupa.com>
2021-02-04 12:20:55 +0530

    Refactored

--- src/main/java/scaffold/utils/PageUtil.java
+++ src/main/java/scaffold/utils/PageUtil.java
@@ -14,9 +14,8 @@ public class PageUtil {
         final String derivedClassName = defaultPackageName + "."
                                         + platformName.toLowerCase() + "."
                                         + tClass.getSimpleName() + platformName;
-        final Class<?> derivedClass;
         try {
-            derivedClass = Class.forName(derivedClassName);
+            final Class<?> derivedClass = Class.forName(derivedClassName);
             return (T) derivedClass.getDeclaredConstructor().newInstance();
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException | ClassNotFoundException e) {
             e.printStackTrace();
